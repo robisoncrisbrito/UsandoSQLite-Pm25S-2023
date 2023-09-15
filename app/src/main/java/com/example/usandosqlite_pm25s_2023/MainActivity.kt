5445a7a19924536b006ad6dccbfbd92179bf3172
robisoncrisbrito <robison.brito@gmail.com>
2023-09-14 21:26:39 -0300

    .

--- app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
+++ app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.usandosqlite_pm25s_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btIncluirOnClick(view: View) {}
+    fun btAlterarOnClick(view: View) {}
+    fun btExcluirOnClick(view: View) {}
+    fun btPesquisarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
 }
