4dc08acc3c52ca25dec8faf8131215c5612d5900
robisoncrisbrito <robison.brito@gmail.com>
2023-10-18 22:35:13 -0200

    versão com camadas

---
+++ app/src/main/java/com/example/usandosqlite_pm25s_2023/database/DatabaseHandler.kt
@@ -0,0 +1,97 @@
+package com.example.usandosqlite_pm25s_2023.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build.VERSION_CODES.P
+import com.example.usandosqlite_pm25s_2023.entity.Pessoa
+import java.lang.StringBuilder
+
+class DatabaseHandler ( context : Context ) : SQLiteOpenHelper ( context, DATABASE_NAME, null, DATABASE_VERSION ) {
+
+    companion object {
+        private val DATABASE_VERSION = 1
+        private val DATABASE_NAME = "dbfile.sqlite"
+        private val TABLE_NAME = "pesso"
+        private val KEY_ID = "_id"
+        private val KEY_NOME = "nome"
+        private val KEY_TELEFONE = "telefone"
+    }
+
+
+    override fun onCreate(bd: SQLiteDatabase?) {
+        bd?.execSQL( "CREATE TABLE IF NOT EXISTS ${TABLE_NAME} ( ${KEY_ID} INTEGER PRIMARY KEY AUTOINCREMENT, ${KEY_NOME} TEXT, ${KEY_TELEFONE} TEXT)" )
+    }
+
+    override fun onUpgrade(bd: SQLiteDatabase?, p1: Int, p2: Int) {
+        bd?.execSQL( "DROP TABLE ${TABLE_NAME}" )
+        onCreate( bd )
+    }
+
+    fun insert( pessoa : Pessoa) {
+        val registro = ContentValues()
+        registro.put( KEY_NOME, pessoa.nome )
+        registro.put( KEY_TELEFONE, pessoa.telefone )
+
+        val bd = this.writableDatabase
+        bd.insert( TABLE_NAME, null, registro )
+    }
+
+    fun update( pessoa : Pessoa) {
+        val registro = ContentValues()
+        registro.put( KEY_NOME, pessoa.nome )
+        registro.put( KEY_TELEFONE, pessoa.telefone )
+
+        val bd = this.writableDatabase
+        bd.update( TABLE_NAME, registro, "_id=${pessoa._id}", null )
+    }
+
+    fun delete( _id : Int) {
+        val bd = this.writableDatabase
+        bd.delete( TABLE_NAME, "_id=${_id}", null )
+    }
+
+    fun find( _id : Int) : Pessoa? {
+        val bd = this.writableDatabase
+        val cursor = bd.query( TABLE_NAME,
+            null,
+            "_id=${_id}",
+            null,
+            null,
+            null,
+            null
+        )
+
+        if ( cursor.moveToNext() ) {
+            val pessoa = Pessoa( _id, cursor.getString( 1 ), cursor.getString( 2 ) )
+            return pessoa
+        } else {
+            return null
+        }
+    }
+
+    fun list() : String {
+        val bd = this.writableDatabase
+
+        val cursor = bd.query( TABLE_NAME,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        val saida = StringBuilder()
+
+        while ( cursor.moveToNext() ) {
+            saida.append( cursor.getString( 1 ) )
+            saida.append( " - ")
+            saida.append( cursor.getString( 2 ) )
+            saida.append( "\n")
+        }
+
+        return saida.toString()
+    }
+}
---
+++ app/src/main/java/com/example/usandosqlite_pm25s_2023/entity/Pessoa.kt
@@ -0,0 +1,3 @@
+package com.example.usandosqlite_pm25s_2023.entity
+
+data class Pessoa (var _id : Int, var nome : String, var telefone : String )
